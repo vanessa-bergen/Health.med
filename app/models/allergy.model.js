11d3a54f5e650aa5300f236c5033533959b030b4
Ubuntu <ubuntu@ip-172-31-44-244.us-west-2.compute.internal>
2015-11-15 19:59:43 +0000

    created allergy model

---
+++ app/models/allergy.model.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AllergySchema = new Schema({
+    name : {
+        type : String,
+        required true
+    },
+
+    type : {
+        type : String,
+        required true
+    },
+
+    severity : {
+        type : String,
+        enum : [
+            'mild',
+            'moderate',
+            'severe'
+        ],
+        required true
+    },
+
+    exposer_type : {
+        type : String,
+        required true
+    });
+});
+
+mongoose.model('Allergy', AllergySchema);
+
+
+
+	
