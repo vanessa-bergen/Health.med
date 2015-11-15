ba925675dbe73cfaf085dc7a4ffb1d585facad01
Ubuntu <ubuntu@ip-172-31-44-244.us-west-2.compute.internal>
2015-11-15 20:05:18 +0000

    adding appointment model

---
+++ app/models/appointment.model.js
@@ -0,0 +1,53 @@
+var mongoose = required('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AppointmentSchema = new Schema({
+    notes: {
+        type : String,
+        required : true
+    },
+    height : {
+        type : Number,
+        required : true
+    },
+    
+    weight : {
+        type : Number,
+        required : true
+    },
+
+    blood_pressure : {
+        type : Number,
+        required : true
+    },
+
+    smoking : {
+        type : Boolean,
+        required : true
+    },
+     
+    date : {
+        day : {
+            type : Number,
+            required : true,
+            min : 1,
+            max : 31
+        },
+        month : {
+            type : Number,
+            required : true,
+            min : 1,
+            max : 12
+        },
+        year : {
+            type : Number,
+            required : true
+        }
+    }
+});
+
+mongoose.model('Appointment', AppointmentSchema);
+
+     
+        
