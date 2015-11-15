b95ef7450033b15a88ad7aec5e39d25dff6afeba
CCMDevel <chris.j.mcgrath@gmail.com>
2015-11-15 15:21:07 -0500

    added new models to db config file

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,10 +5,13 @@ var mongoose = require('mongoose');
 module.exports = function(){
     var db = mongoose.connect('mongodb://localhost/health_med_dev');
 
+    require('../app/models/allergy.model.js');
+    require('../app/models/appointment.model.js');
+    require('../app/models/condition.model.js');
     require('../app/models/doctor.model.js');
     require('../app/models/medication.model.js');
     require('../app/models/patient.model.js');
-    require('../app/models/symptom.model.js');    
+    require('../app/models/symptom.model.js');
     require('../app/models/test_result.model.js');
     require('../app/models/treatment.model.js');
 
