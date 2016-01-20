d34b51b873c12044710f5392e3845145194062ee
CCMDevel <chris.j.mcgrath@gmail.com>
2016-01-19 21:07:18 -0500

    added ability to add symptoms to your allergies

--- public/angular/controllers/allergy.controller.js
+++ public/angular/controllers/allergy.controller.js
@@ -37,7 +37,21 @@ angular.module('module_patient')
     $scope.model.patient = {};
 
     $scope.model.newAllergy = {
-        allergy : {}    
+        allergy : {
+            symptoms : {}
+        },
+        addSymptom : function(symptom){
+            if (symptom._id){
+                $scope.model.newAllergy.allergy.symptoms[symptom._id] = symptom;
+            }
+            console.log(JSON.stringify($scope.model.newAllergy.allergy));
+        },
+        removeSymptom : function(symptom){
+            if (symptom._id && $scope.model.newAllergy.allergy.symptoms[symptom._id]){
+                delete $scope.model.newAllergy.allergy.symptoms[symptom._id];
+            }
+            console.log(JSON.stringify($scope.model.newAllergy.allergy));
+        }
     };
 
     $scope.view = {};
@@ -53,43 +67,72 @@ angular.module('module_patient')
         }
     };
 
-    $scope.http = {
-        patient : {
-            get : function(){
-                console.log('patient.get()');
+    $scope.http = {};
+    
+    $scope.http.patient = {
+        get : function(){
+            console.log('http.patient.get()');
 
-                $http.get(ENDPOINT + "/patient/me")
-                .success(function(res){
-                    console.log("    patient.get -> success");
-                    console.log(JSON.stringify(res.patient));
+            $http.get(ENDPOINT + "/patient/me")
+            .success(function(res){
+                console.log("http.patient.get -> success");
 
-                    $scope.model.patient = res.patient;
-                })
-                .error(function(){
-                    console.log("    patient.get -> failure");                        
-                });
-            },
-            allergies : {
-                add : function(newAllergy){
-                    console.log('patient.allergy.add()');
-                    
-                    $http.put(ENDPOINT + "/patient/allergy/add", $scope.model.newAllergy)
-                    .success(function(patient){
-                        console.log('    patient.allergies.add -> success');
-                        
-                        $scope.view.controller.toggleNewAllergy();
-                        $scope.model.patient = patient;
-                    })
-                    .error(function(err){
-                        console.log('    patient.allergies.add -> error');
-                        console.log(JSON.stringify(err));
-                    });
-                }
-            }, 
+                $scope.model.patient = res.patient;
+            })
+            .error(function(){
+                console.log("http.patient.get -> failure");                        
+            });
         }
     };
 
+    $scope.http.allergies = {
+        add : function(newAllergy){
+            console.log('http.allergies.add()');
+            var list = [];
+            for (var id in $scope.model.newAllergy.allergy.symptoms){
+                console.log("  " + id);
+                if ($scope.model.newAllergy.allergy.symptoms.hasOwnProperty(id)){
+                    console.log("    " + id);
+                    list.push(id);
+                }
+            }
+
+            $scope.model.newAllergy.allergy.symptoms = list;
+            console.log("    " + JSON.stringify($scope.model.newAllergy.allergy));
+
+            $http.put(ENDPOINT + "/patient/allergy/add", $scope.model.newAllergy)
+            .success(function(patient){
+                console.log('http.allergies.add -> success');
+                
+                $scope.view.controller.toggleNewAllergy();
+                $scope.model.patient = patient;
+            })
+            .error(function(err){
+                console.log('http.allergies.add -> error');
+                console.log(JSON.stringify(err));
+            });
+        }
+    }; 
+    
+    $scope.http.symptoms = {
+        get : function(){
+            console.log('http.symptoms.get()');
+
+            $http.get(ENDPOINT + "/symptom?sort=true")
+            .success(function(symptoms){
+                console.log('http.symptoms.get -> success');
+
+                $scope.model.symptoms = symptoms;
+            })
+            .error(function(err){
+                console.log('http.symptoms.get() -> error');
+                console.log(JSON.stringify(err));
+            });
+        }        
+    };
+
     $scope.http.patient.get();
+    $scope.http.symptoms.get();
 });
 
 console.log('allergy controller loaded');
