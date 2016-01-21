4daacf0206ba48da44c569a93e4a249d70d64735
CCMDevel <chris.j.mcgrath@gmail.com>
2016-01-21 16:19:42 -0500

    updated test_result model and routes to match CBC result

--- app/routes/test_result.routes.js
+++ app/routes/test_result.routes.js
@@ -2,11 +2,15 @@ module.exports = function(app){
     var testresult_controller = require('../controllers/test_result.controller.js')();
     app.post('/testresult', testresult_controller.create);
 
-    app.get('/testresult/:testresult_id', testresult_controller.get);
+    app.get('/testresult/patient_id/:patient_id', testresult_controller.findByPatientId);
+    app.get('/testresult/id/:testresult_id', testresult_controller.get);
+    
 
     app.get('/testresult', testresult_controller.index);
 
     app.param('testresult_id', testresult_controller.findById);
+    app.param('patient_id', testresult_controller.setPatientId);
+
     console.log('        test result routes initialized');
 
 }
