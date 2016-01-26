b965c2958adfd2613b8665d81c9351f2f4a2c0a5
CCMDevel <chris.j.mcgrath@gmail.com>
2016-01-26 15:48:30 -0500

    removed dependency on ngRoute and switched for ui.router

--- public/angular/modules/home.module.js
+++ public/angular/modules/home.module.js
@@ -1,21 +1,44 @@
 console.log("Welcome to Health.med");
 
-angular.module('module_login', ['ngRoute', 'module_config'])
-.config(function($routeProvider){
-    $routeProvider.when('/signup', {
-        templateUrl : "/views/signup.html"
-    });
+angular.module('module_login', [
+    'ui.router', 
+    'module_config'
+])
+.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/login');
 
-    $routeProvider.when('/for_patients', {
-        templateUrl : "/views/for_patients.html"
-    });
+    $stateProvider
+        .state('signup', {
+            url : "/signup",
+            templateUrl : '/views/signup.html'
+        })
+        .state('for_patients', {
+            url : "/for_patients",
+            templateUrl : '/views/for_patients.html'
+        })
+        .state('for_doctors', {
+            url : "/for_doctors",
+            templateUrl : '/views/for_doctors.html'
+        })
+        .state('login', {
+            url : '/login',
+            templateUrl : '/views/login.html'
+        });
 
-    $routeProvider.when('/for_doctors', {
-        templateUrl : "/views/for_doctors.html"
-    });
+    // $routeProvider.when('/signup', {
+    //     templateUrl : "/views/signup.html"
+    // });
 
-    $routeProvider.otherwise({
-        templateUrl : "/views/login.html"
-    });
+    // $routeProvider.when('/for_patients', {
+    //     templateUrl : "/views/for_patients.html"
+    // });
+
+    // $routeProvider.when('/for_doctors', {
+    //     templateUrl : "/views/for_doctors.html"
+    // });
+
+    // $routeProvider.otherwise({
+    //     templateUrl : "/views/login.html"
+    // });
 });
 
