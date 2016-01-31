b430049912a1728ef73fd8b5ad24a711d8d879f7
root <root@ip-172-31-44-244.us-west-2.compute.internal>
2016-01-31 20:22:19 +0000

    patientprofile controller

--- public/angular/controllers/patient/profile.controller.js
+++ public/angular/controllers/patient/profile.controller.js
@@ -1,6 +1,20 @@
 console.log('ctrlr_profile');
 
 angular.module('module_patient')
-.controller('ctrlr_profile', function($scope, $http, $location, $window, ENDPOINT){
-    $scope.hello = "profile";    
+.controller('ctrlr_profile', function($scope, $http, $location, $window, ENDPOINT, httpPatient){
+    $scope.hello = "Profile"; 
+	
+	$scope.model = {};
+	$scope.model.profile = {};
+	
+	
+	httpPatient.getMe().success(function(me){
+		console.log('httpPatient.getMe -> success');
+		$scope.model.profile.patient = me;
+	})
+		.error(function(err){
+		console.log('httpPatient.getMe -> error');
+        console.log(JSON.stringify(err));	
+	});
 });
+
