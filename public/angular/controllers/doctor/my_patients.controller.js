a47b2f3d944ecc0c2856b44ff8985d3444c68bca
CCMDevel <chris.j.mcgrath@gmail.com>
2016-02-06 01:41:17 -0500

    doctor querying patients and displaying results

--- public/angular/controllers/doctor/my_patients.controller.js
+++ public/angular/controllers/doctor/my_patients.controller.js
@@ -21,12 +21,54 @@ angular.module('module_doctor')
 		},
 		setCurrentTab : function(i){
 			$scope.view.model.currentView = i;
-		}
+		},
+        isQueryEnabled : function(){ 
+            if (!$scope.model.patient.queryParams.name_first && 
+                !$scope.model.patient.queryParams.name_last){
+                return false;
+            
+            } else if ($scope.model.patient.queryParams.name_first &&
+                       $scope.model.patient.queryParams.name_last){
+                if (!angular.isUndefined($scope.model.patient.query.name_first) && !angular.isUndefined($scope.model.doctor.query.name_last)){
+                    return $scope.model.patient.query.name_first.length >= 1 &&
+                            $scope.model.patient.query.name_last.length >= 1;
+                } else {
+                    return false;
+                }
+            } else if ($scope.model.patient.queryParams.name_first){
+                if (!angular.isUndefined($scope.model.patient.query.name_first)){
+                    return $scope.model.patient.query.name_first.length >= 1;
+                } else {
+                    return false;
+                }  
+            } else if (!angular.isUndefined($scope.model.patient.query.name_last)){
+                return $scope.model.patient.query.name_last.length >= 1;
+            } else {
+                return false;
+            }
+        },
+        hasSecondaryPhone : function(patient){
+        	var isUndefined = angular.isUndefined(patient.phone_number.secondary);
+        	return isUndefined;
+        }
 	}
 
 	$scope.goToPatient = function(patient_id){
 		console.log('goToPatient(' + patient_id + ')');
 	}
+
+	$scope.queryPatient = function(query){
+		httpPatient.queryPatient(query).success(function(patients){
+            console.log('httpPatient.queryPatient -> success');
+
+            // filter patients that doctor already has access to
+
+            $scope.model.patient.queryResults = patients;
+		}).error(function(err){
+            console.log('httpPatient.queryPatient -> error');
+            console.log(JSON.stringify(err));
+		});
+	};
 
     httpDoctor.getMe().success(function(doctor){
         console.log('httpDoctor.getMe -> success');
